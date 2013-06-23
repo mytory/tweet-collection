5b39158c1312e327acb9f0d8ddfbf373d7944fa6
mytory <mytory@gmail.com>
2013-06-23 14:39:14 +0900

    트위터 RSS 피트 추가. 링크를 트위터 안에서 소개하고 있는 링크로 했음. 따라서 RSS 링크를 클릭할 시 트위터 페이지로 오지 않고 트위터에서 추천한 링크로 이동하게 됨.

--- tweet-collection.php
+++ tweet-collection.php
@@ -340,7 +340,13 @@ if (get_option('tweet-collection-title-length') AND get_option('tweet-collection
 
 function tc_rss_template ( $archive_template ) {
      if (is_post_type_archive ('tweet') AND $_GET['rss'] == 'for-fb') {
-        return dirname( __FILE__ ) . '/tc-rss-for-fb.php';
+        $args = array(
+            'post_type' => 'tweet',
+            'posts_per_page' => 20,
+        );
+        $wp_query = new WP_Query($args);
+        $archive_template = dirname(__FILE__) . '/tc-rss-for-fb.php';
+        return $archive_template;
      }
 }
 add_filter( 'archive_template', 'tc_rss_template' ) ;
